79e7a92454e2b9cc4bf1e873fe413d8a40e0df9c
Jeff Cogswell <jeffcogs@gmail.com>
2020-09-14 15:25:08 -0400

    Added Pet/Art Store

---
+++ ArtStore/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PetStore.Models;
+
+namespace PetStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            /*Product prod = Product.Create("Kitty Litter", 13.95m);
+            return Content($"{prod.id} {prod.Name} {prod.Price}");*/
+
+
+            /*Product prod = Product.Read(2);
+            prod.Price = 19.00m;
+            prod.Save();
+            return Content($"{prod.id} {prod.Name} {prod.Price}");*/
+
+            /*Product.Delete(5);
+            return Content("OK");*/
+
+
+            /*List<Product> prods = Product.Read();
+            return Content(prods.Count.ToString());
+            */
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ArtStore/Models/Product.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace PetStore.Models
+{
+    [Table("Product")]
+    public class Product
+    {
+        [Key]
+        public long id { get; set; }
+
+        public string Name { get; set; }
+        public Decimal Price { get; set; }
+
+        public static Product Create(string _name, Decimal _price)
+        {
+            // Create the new Product instance
+            Product prod = new Product() { Name = _name, Price = _price };
+
+            // Save it to the database
+            IDbConnection db = new SqlConnection("Server=.;Database=devbuildjoin;user id=sa;password=abc123;");
+            long _id = db.Insert<Product>(prod);
+
+            // Set the id column of the instance
+            prod.id = _id;
+
+            // Return the instance
+            return prod;
+        }
+
+        public static Product Read(long _id)
+        {
+            IDbConnection db = new SqlConnection("Server=.;Database=devbuildjoin;user id=sa;password=abc123;");
+            Product prod = db.Get<Product>(_id);
+            return prod;
+        }
+
+        public static List<Product> Read()
+        {
+            IDbConnection db = new SqlConnection("Server=.;Database=devbuildjoin;user id=sa;password=abc123;");
+            List<Product> prods = db.GetAll<Product>().ToList();
+            return prods;
+        }
+
+        public static void Delete(long _id)
+        {
+            IDbConnection db = new SqlConnection("Server=.;Database=devbuildjoin;user id=sa;password=abc123;");
+            db.Delete(new Product() { id = _id });
+        }
+
+        public void Save()
+        {
+            IDbConnection db = new SqlConnection("Server=.;Database=devbuildjoin;user id=sa;password=abc123;");
+            db.Update(this);
+        }
+    }
+}
